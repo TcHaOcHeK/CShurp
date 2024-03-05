1c8d90b88b0ad9ad9cf84f4a34f68aca83fc1026
TcHaOcHeK <160489385+TcHaOcHeK@users.noreply.github.com>
2024-03-05 15:28:05 +0600

    New project

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Lab_1_WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(66, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 80);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(40);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 50;
+            this.dataGridView1.Size = new System.Drawing.Size(380, 348);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Your score:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Timers.Timer timer1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Panel panel1;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Lab_1_WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        const int coll = 8, row = 8;
+        Random rand = new Random();
+        Color[] pall = { Color.Red, Color.Green, Color.Yellow, Color.Blue };
+        int col = 0, score = 0;
+        
+        int[,] desk = new int[coll, row];
+        int[,] board = new int[coll, row];
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnCount = coll;
+            dataGridView1.RowCount = row;
+            col = rand.Next(4);
+            panel1.BackColor = pall[col];
+        }
+
+        public int ThinkColor(int y, int x, int color,  int count = 0)
+        {
+            int sum = 0;
+            if (y < coll && y >= 0 && x < row && x >= 0)
+            {
+                if (desk[y, x] == color || count == 0)
+                {
+                    board[y, x] = 1;
+                    if(count != 2)
+                        sum += ThinkColor(y + 1, x, color, 1);
+                    if (count != 1)
+                        sum += ThinkColor(y - 1, x, color, 2);
+                    if(count!= 4)
+                        sum += ThinkColor(y, x + 1, color, 3);
+                    if(count!= 3) 
+                        sum += ThinkColor(y, x - 1, color, 4);
+                    textBox1.Text += ' ' + Convert.ToString(sum);
+                    return sum + 1;
+                    
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        private void Zakras(int y, int x)
+        {
+            int TC = ThinkColor(y, x, desk[y, x]);
+            //textBox1.Text = Convert.ToString(TC); //For debug
+            
+            for (int i = 0; i < coll; i++)
+                for (int j = 0; j < row; j++)
+                    if (board[i, j] == 1 && TC >= 3)
+                    {
+                        dataGridView1[i, j].Style.BackColor = Color.White;
+                        desk[i, j] = 0;
+                        board[i, j] = 0;
+                        score ++;
+                    }
+            
+                    else
+                    {
+                        board[i, j] = 0;
+                    }
+            
+
+
+        }
+
+     
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int x = e.RowIndex, y = e.ColumnIndex;
+            if (desk[y, x] == 0)
+            {
+                
+                dataGridView1[y, x].Style.BackColor =panel1.BackColor;
+                desk[y, x] = col + 1;
+                Zakras(y, x);
+                col = rand.Next(4);
+                panel1.BackColor = pall[col];
+                textBox1.Text =  "Your score:" + Convert.ToString(score);
+
+            }
+            
+
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+        }
+
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            for (int k = 0; k < 200; k++)
+            {
+                int x = rand.Next(row), y = rand.Next(coll);
+                if (desk[x, y] == 0)
+                {
+                    int i = rand.Next(4);
+                    dataGridView1[x, y].Style.BackColor = pall[i];
+                    desk[x, y] = i + 1;
+                    return;
+                }
+            }
+        }
+        
+    }
+}
