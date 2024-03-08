5aac5a536ad1790a551ad0aff86f4097f485811e
TcHaOcHeK <160489385+TcHaOcHeK@users.noreply.github.com>
2024-03-08 15:35:44 +0600

    add new form

---
+++ Form2.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Lab_1_WindowsForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
